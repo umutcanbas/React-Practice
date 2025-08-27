ae1c0086872806d650a11b4aac193be0ba658392
umutcanbas <umutbas57@gmail.com>
2025-08-27 19:09:55 +0300

    Tailwind giriş

--- tailwind-giris/src/app/app.tsx
+++ tailwind-giris/src/app/app.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+type Props = {
+  emoji?: string;
+  title?: string;
+  dec?: string;
+};
 
-export const app = () => {
+const Card = ({ emoji, title, dec }: Props) => {
   return (
-    <div>app</div>
-  )
-}
+    <div className="m-4 shadow p-4 flex flex-col gap-4 round ">
+      <span className="text-4xl">{emoji}</span>
+      <span>{title}</span>
+      <span className="text-gray-400">{dec}</span>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Card emoji="🍵" title="Çay" dec="Orta boy" />
+      <Card emoji='☕️' title="Americano" dec="Büyük boy" />
+      <Card emoji='🍰' title="Pasta" dec="Cikolatalı" />
+    </>
+  );
+};
+
+export default App;
