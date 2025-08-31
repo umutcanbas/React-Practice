41783ef9df578dd60bee0fdc6e08fe5eab20de4c
umutcanbas <umutbas57@gmail.com>
2025-08-31 15:32:38 +0300

    Refactor App component to fetch and display users and their todos

--- redux-toolkit-example/src/app/app.tsx
+++ redux-toolkit-example/src/app/app.tsx
@@ -1,5 +1,24 @@
-const app = () => {
-  return <div>app</div>;
+import { useGetUsersQuery, useGetTodosQuery, useGetUserQuery } from "./api";
+
+const App = () => {
+  const { data: users } = useGetUsersQuery();
+  const { data: todos } = useGetTodosQuery();
+  const { data: user } = useGetUserQuery(4);
+
+  console.log(user);
+
+  return <div>
+    {users?.map((user) => <li key={user.id}>{user.name}</li>)}
+    {user && <div>
+      <h2>{user.name}</h2>
+      <h3>Todos:</h3>
+      <ul>
+        {todos?.filter((todo) => todo.userId === user.id).map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>}
+  </div>;
 };
 
-export default app;
+export default App;
