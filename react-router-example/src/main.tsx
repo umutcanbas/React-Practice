6235067885f3ac7c9a6edd6d532eef033c4bb933
umutcanbas <umutbas57@gmail.com>
2025-09-02 16:02:42 +0300

    Add MainLayout component and update routing structure

--- react-router-example/src/main.tsx
+++ react-router-example/src/main.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "app/app";
+import About from "app/About";
+import Projects from "app/Projects";
+import MainLayout from "app/MainLayout";
+import Users from "app/Users";
+import UserDetail from "app/UserDetail";
+import UserAdd from "app/UserAdd";
+import UserEdit from "app/UserEdit";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:id" element={<UserDetail />} />
+          <Route path="/users/add" element={<UserAdd />} />
+          <Route path="/users/:id/edit" element={<UserEdit />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
