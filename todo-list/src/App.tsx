d357b54845a0ab61a04bfcb3c8bdb18588a16184
umutcanbas <umutbas57@gmail.com>
2025-09-05 17:18:35 +0300

    Refactor App component to include Navbar, TaskList, TomorrowPreview, and BottomNavigation

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,5 +1,17 @@
+import BottomNavigation from "./components/BottomNavigation";
+import Navbar from "./components/Navbar";
+import TaskList from "./components/TaskList";
+import TomorrowPreview from "./components/TomorrowPreview";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return (
+    <div className="bg-orange-50 pb-24">
+      <Navbar />
+      <TaskList />
+      <TomorrowPreview />
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default App;
