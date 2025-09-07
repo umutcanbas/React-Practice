c4c478124fc1f59a05c0ec70c15e96e4d0ba8b91
umutcanbas <umutbas57@gmail.com>
2025-09-07 22:54:34 +0300

    Refactor App component structure and update main.tsx imports

--- supabase-auth/src/App.tsx
+++ supabase-auth/src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import LogIn from "./components/LogIn";
+import LogOut from "./components/LogOut";
+import ProductAdd from "./components/ProductAdd";
+import Register from "./components/Register";
+import UserStatus from "./components/UserStatus";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  const [sessionUserId, setSessionUserId] = useState<string | null>(null);
+
+  return (
+    <div className="h-screen bg-slate-400">
+      <LogIn />
+      <Register />
+      <LogOut />
+      <UserStatus
+        setSessionUserId={setSessionUserId}
+        sessionUserId={sessionUserId}
+      />
+      <ProductAdd sessionUserId={sessionUserId} />
+    </div>
+  );
 };
 
 export default App;
