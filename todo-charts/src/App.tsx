0b22cdef3323b358b1b3caeafe303ffefa544c6a
umutcanbas <umutbas57@gmail.com>
2025-09-07 13:58:07 +0300

    Refactor App and main components for improved structure and readability

--- todo-charts/src/App.tsx
+++ todo-charts/src/App.tsx
@@ -1,5 +1,17 @@
+import BottomNavigation from "./components/BottomNavigation";
+import GridChart from "./components/GridChart";
+import Header from "./components/Header";
+import ResultChart from "./components/ResultChart";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return (
+    <div className="h-screen bg-orange-100 flex flex-col gap-4">
+      <Header />
+      <GridChart />
+      <ResultChart />
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default App;
--- todo-charts/src/main.tsx
+++ todo-charts/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Filler,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
 
-createRoot(document.getElementById('root')!).render(
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
