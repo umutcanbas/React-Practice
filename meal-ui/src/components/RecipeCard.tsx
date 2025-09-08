6662637a97685e70eb5571ccfe6bfd5f2895ebd7
umutcanbas <umutbas57@gmail.com>
2025-09-09 01:42:02 +0300

    Add RecipeCard component to display recipe details with images and ratings

---
+++ meal-ui/src/components/RecipeCard.tsx
@@ -0,0 +1,40 @@
+import { Clock, Star } from "lucide-react";
+import bg1 from "../assets/chicken.jpeg";
+import bg2 from "../assets/crepes.jpeg";
+
+type Props = {
+  id: string;
+};
+
+const RecipeCard = ({ id }: { id: string }) => {
+  return (
+    <div className="felx flex-col gap-4">
+      <div
+        className="flex h-60 flex-col justify-between gap-4 rounded-2xl p-4"
+        style={{
+          backgroundImage: `url(${id == "1" ? bg1 : bg2})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="flex items-center justify-center gap-1 self-start rounded-full bg-black/30 p-2 px-3 py-1 text-white">
+          <Clock size={12} />
+          <span className="text-sm">15 mins</span>
+        </div>
+
+        <div className="flex items-center justify-center gap-1 self-end rounded-full bg-yellow-400 px-6 py-1 text-black">
+          <Star fill="black" size={16} />
+          <span className="text-base">4.8</span>
+        </div>
+      </div>
+      <div className="gap-0s flex flex-col">
+        <span className="text-base font-semibold text-gray-500">
+          Chicken Curry
+        </span>
+        <span className="text-base font-semibold text-gray-500">Asian</span>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
