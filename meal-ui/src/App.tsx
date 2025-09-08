6de9203d0132e89f46634a329c29abaa288a3210
umutcanbas <umutbas57@gmail.com>
2025-09-09 01:41:42 +0300

    Refactor App component layout for improved structure and readability

--- meal-ui/src/App.tsx
+++ meal-ui/src/App.tsx
@@ -1,5 +1,21 @@
+import BottomNavigation from "./components/BottomNavigation";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import PopularRecipes from "./components/PopularRecipes";
+import SearchBar from "./components/SearchBar";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return (
+    <div className="flex h-screen flex-col gap-4">
+      <Header />
+      <Hero />
+      <SearchBar />
+      <Categories />
+      <PopularRecipes />
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default App;
