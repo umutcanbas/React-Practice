b05fabfab66dce36db691206194af2fff868ef44
umutcanbas <umutbas57@gmail.com>
2025-09-08 20:21:03 +0300

    Add product fetching logic and integrate ProductList component in App

--- supabase-auth/src/App.tsx
+++ supabase-auth/src/App.tsx
@@ -4,9 +4,31 @@ import LogOut from "./components/LogOut";
 import ProductAdd from "./components/ProductAdd";
 import Register from "./components/Register";
 import UserStatus from "./components/UserStatus";
+import ProductList, { type Product } from "./components/ProductList";
+import { supabase } from "./supabase";
 
 const App = () => {
   const [sessionUserId, setSessionUserId] = useState<string | null>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getProducts = async (): Promise<Product[]> => {
+    if (!sessionUserId) {
+      setProducts([]);
+      return [];
+    }
+    const { data, error } = await supabase
+      .from("products")
+      .select()
+      .eq("user_id", sessionUserId)
+      .order("created_at", { ascending: false });
+
+    if (error || !data) {
+      setProducts([]);
+      return [];
+    }
+    setProducts(data);
+    return data;
+  };
 
   return (
     <div className="h-screen bg-slate-400">
@@ -17,7 +39,12 @@ const App = () => {
         setSessionUserId={setSessionUserId}
         sessionUserId={sessionUserId}
       />
-      <ProductAdd sessionUserId={sessionUserId} />
+      <ProductAdd getProduct={getProducts} sessionUserId={sessionUserId} />
+      <ProductList
+        products={products}
+        getProduct={getProducts}
+        sessionUserId={sessionUserId}
+      />
     </div>
   );
 };
