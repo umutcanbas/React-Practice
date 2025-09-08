897be6e5d5cf256c21a5a27d32db1d4d4e9ee5e2
umutcanbas <umutbas57@gmail.com>
2025-09-09 01:41:59 +0300

    Add Hero component for welcoming users with a personalized message

---
+++ meal-ui/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-0 px-4">
+      <span className="text-grey-500 text-sm">Hello Teresa</span>
+      <span className="text-2xl font-semibold">Make your own food, </span>
+      <span className="text-2xl font-semibold">
+        stay at <span className="text-yellow-400">home</span>
+      </span>
+    </div>
+  );
+};
+
+export default Hero;
