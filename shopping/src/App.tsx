e1f2fb52bdaf25c0bba8cd0b547c7627bbe13a32
umutcanbas <umutbas57@gmail.com>
2025-09-09 16:09:21 +0300

    Refactor App component to include layout structure with navigation and content components

--- shopping/src/App.tsx
+++ shopping/src/App.tsx
@@ -1,5 +1,21 @@
+import BottomNavigation from "./components/BottomNavigation";
+import BreadCrumb from "./components/BreadCrumb";
+import Categories from "./components/Categories";
+import NavBar from "./components/NavBar";
+import Prodcuts from "./components/Prodcuts";
+import SearchBar from "./components/SearchBar";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return (
+    <div className="bg-gray-100">
+      <NavBar />
+      <SearchBar />
+      <BreadCrumb />
+      <Categories />
+      <Prodcuts />
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default App;
