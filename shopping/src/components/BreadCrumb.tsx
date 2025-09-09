309cc9de0d12711623318b0859068bef8e30e655
umutcanbas <umutbas57@gmail.com>
2025-09-09 16:09:29 +0300

    Add BreadCrumb component for navigation clarity

---
+++ shopping/src/components/BreadCrumb.tsx
@@ -0,0 +1,11 @@
+const BreadCrumb = () => {
+  return (
+    <div className="flex gap-2 px-4 py-2">
+      <span className="text-gray-500">Home</span>
+      <span className="text-gray-500">/</span>
+      <span>Shop</span>
+    </div>
+  );
+};
+
+export default BreadCrumb;
