4f325ea2b6bb6a20f783a23ddc68d041d8b4627f
umutcanbas <umutbas57@gmail.com>
2025-09-10 16:00:23 +0300

    Refactor App component to include Navigation, SearchBar, Categories, Products, and BottomNavigation

--- fresh-direct/src/App.tsx
+++ fresh-direct/src/App.tsx
@@ -1,5 +1,19 @@
+import Navigation from "./components/Navigation";
+import SearchBar from "./components/SearchBar";
+import Categories from "./components/Categories";
+import Products from "./components/Products";
+import BottomNavigation from "./components/BottomNavigation";
+
 const App = () => {
-  return <div className="p-4 text-3xl">App</div>;
+  return (
+    <div className="min-h-screen mx-auto relative">
+      <Navigation />
+      <SearchBar />
+      <Categories />
+      <Products />
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default App;
