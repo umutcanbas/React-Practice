20b0b50620388665d6a527150acc406a81c4674c
umutcanbas <umutbas57@gmail.com>
2025-10-05 13:44:26 +0300

    Implement product fetching and display in the main page component

--- next-data-fetching/app/page.tsx
+++ next-data-fetching/app/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import ProductCard from "./_components/ProductCard";
 
-const page = () => {
-  return <div>page</div>;
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+const getProducts = async (): Promise<Product[]> => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const res = await fetch(API_URL);
+  const products = await res.json();
+  return products;
+};
+
+const page = async () => {
+  const products = await getProducts();
+  return (
+    <ul className="grid max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 place-items-center mx-auto ">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </ul>
+  );
 };
 
 export default page;
